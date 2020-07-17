5336d93cd4986c623b6d0cfaeb3fadeabdba03f6
JeanWu <jean.wu@ischool.com.tw>
2020-07-17 09:58:56 +0800

    增加科目排序邏輯 (領域 (領域中英對照)>科目(科目中英對照管理))

--- ESL_System/Model/CourseInfo.cs
+++ ESL_System/Model/CourseInfo.cs
@@ -11,16 +11,18 @@ namespace ESL_System.Model
     /// </summary>
     class CourseInfo
     {
-        public CourseInfo(string subject ,string domain,int order) {
-
-            this.DomainOrder = 99999;
+        public CourseInfo(string subject, string domain, int domainOrder, int subjectOrder)
+        {
+            this.DomainOrder = 99999; // 初始化 若沒有在領域排序對照表之領域 排在最後面
+            this.SubjectOrder = 99999;
             this.Subject = subject;
             this.Domain = domain;
-            this.DomainOrder = order;
+            this.DomainOrder = domainOrder;
+            this.SubjectOrder = subjectOrder;
         }
-        public string Subject  { get; set; }
+        public string Subject { get; set; }
         public string Domain { get; set; }
-        public int DomainOrder  { get; set; } 
-
+        public int DomainOrder { get; set; }
+        public int SubjectOrder { get; set; }
     }
 }
